f1b85c812392131e1da5b7387a1b91d884f5eb1f
Aarthi333 <82799431+Aarthi333@users.noreply.github.com>
2021-05-25 19:28:25 +0530

    Add files via upload

---
+++ sql files/day27Pizza.sql
@@ -0,0 +1,10 @@
+select * from Products
+delete Products
+
+insert into Products values('cheese pizza',100,10,'Extra cheesy and tasty!')
+
+select * from customers
+
+select * from bookings
+
+select * from Orders
